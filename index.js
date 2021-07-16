77fa77398e6a30beed2d7aa09533101197cc279f
yunus <yeyilmaz34@gmail.com>
2021-07-16 15:30:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+
+let toggle = document.querySelector('.toggle');
+let topbar = document.querySelector('.topbar');
+let navigation = document.querySelector('.navigation');
+let main = document.querySelector('.main');
+let themeSwitch = document.querySelector('.themeSwitch')
+let body = document.querySelector('body')
+
+
+
+toggle.onclick = function () {
+    toggle.classList.toggle('active');
+    topbar.classList.toggle('active');
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+}
+
+//theme switch toggle
+
+themeSwitch.onclick = function () {
+    body.classList.toggle('dark')
+}
+
+
+//function for fixing menu active class issue
+function toggleMenu() {  
+    let navigation = document.querySelector('.navigation');
+    let main = document.querySelector('.main');
+    navigation.classList.remove('active');
+    main.classList.remove('active');
+}
